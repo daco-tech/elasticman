56598f143204050668d26931f31aa80e640cc3e7
Daniel Costa <git@danielcosta.pt>
2019-11-13 12:04:49 +0000

    Run delete as a CLI flag

--- main.go
+++ main.go
@@ -15,9 +15,62 @@ func main() {
 	app.Usage = "Elastic Maintenance Tool"
 	app.Version = "3,1415926535897932384626433" //yes, it Pi :)
 
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config, c",
+			Usage: "Load configuration from `FILE`",
+		},
+		cli.StringFlag{
+			Name:  "delete",
+			Value: "no",
+			Usage: "set --delete yes to activate the Delete Option",
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
 		log.Println("Elastic Maintenance Tool")
-		setup()
+		log.Println("Starting ElasticMan...")
+		configFile := "config.json"
+		if c.NArg() > 0 {
+			configFile = c.Args()[0]
+		}
+		//Config
+		if c.String("config") != "" {
+			configFile = c.String("config")
+		}
+		log.Println("--> Loading Configs...")
+		config, cfgErr := general.LoadConfiguration(configFile)
+		if cfgErr != nil {
+			log.Fatalln("No configuration file found. Looking for '" + configFile + "'.")
+		}
+		//Set verbose mode
+		if config.Log.Verbose {
+			log.Println("*** VERBOSE LOG ACTIVATED ***")
+		}
+
+		//Set run pre-conditions
+		var clStatus, err = elastic.GetClusterStatus(config.Elasticsearch.Host, config.Log.Verbose)
+
+		if (config.Elasticsearch.RequiredStatus != "" && config.Elasticsearch.RequiredStatus != clStatus.Status) || (clStatus.NumberOfPendingTasks > config.Elasticsearch.MaxNumberOfPendingTasks) {
+			log.Fatalln("Not in the desired status. Exiting Execution.")
+		}
+
+		if err == "" && config.Log.Verbose {
+			log.Println("Cluster Status: " + clStatus.Status)
+		}
+		var do_something bool = false
+		//Run Actions
+		if c.String("delete") == "yes" {
+			log.Println("DELETE MODE ACTIVATED")
+			do_something = true
+			deleteAction(config)
+		}
+
+		//If nothing runs say something
+		if !do_something {
+			log.Fatalln("No action selected for execution! Nothing to do!")
+		}
+
 		return nil
 	}
 	clierr := app.Run(os.Args)
@@ -26,30 +79,8 @@ func main() {
 	}
 
 }
-func setup() {
 
-	log.Println("Starting ElasticMan...")
-	log.Println("--> Loading Configs...")
-	config, _ := general.LoadConfiguration("config.json")
-	if config.Log.Verbose {
-		log.Println("*** VERBOSE LOG ACTIVATED ***")
-	}
-
-	var clStatus, err = elastic.GetClusterStatus(config.Elasticsearch.Host, config.Log.Verbose)
-
-	if (config.Elasticsearch.RequiredStatus != "" && config.Elasticsearch.RequiredStatus != clStatus.Status) || (clStatus.NumberOfPendingTasks > config.Elasticsearch.MaxNumberOfPendingTasks) {
-		log.Fatalln("Not in the desired status. Exiting Execution.")
-	}
-
-	if err == "" && config.Log.Verbose {
-		log.Println("Cluster Status: " + clStatus.Status)
-	}
-	action(config)
-
-}
-
-func action(config general.Config) {
-
+func deleteAction(config general.Config) {
 	var parsed_indices, _ = elastic.GetParsedIndices(config.Elasticsearch.Host, config.Log.Verbose, config.Parser.DateFormat, config.Parser.DateIndexLastChars, config.Parser.Loglevels, config.Parser.Logtypes)
 	if config.Actions.Delete.Enabled {
 		for _, deletions := range config.Actions.Delete.Todo {
@@ -57,9 +88,4 @@ func action(config general.Config) {
 		}
 
 	}
-
-	//var index = "indexname"
-	//var result = elastic.DeleteIndex(config.ElasticSearch.Host, index, config.Log.Verbose)
-	//log.Println("Delete Index: "+index+"; Result: ", result)
-
 }
